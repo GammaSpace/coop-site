3a2134500133f0d1c8c294ad8b8633c7739864a0
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-06-07 16:56:55 -0400

    Fix rendering of screenshots

--- src/routes/games/[slug]/+page.svelte
+++ src/routes/games/[slug]/+page.svelte
@@ -1,32 +1,31 @@
 <script lang="ts">
-  import SvelteMarkdown from 'svelte-markdown';
-  import { marked, Renderer } from 'marked';
-  import { onMount } from 'svelte';
+  import SvelteMarkdown from "svelte-markdown";
+  import { marked, Renderer } from "marked";
+  import { onMount } from "svelte";
 
   /** @type {import('./$types').PageData} */
-	export let data;
+  export let data;
 
   const defaultRenderer = new Renderer();
-
   let headerWidth: number;
-  let screenshots:string = "";
-  let links:string = "";
+  let screenshots: string = "";
+  let links: string = "";
   let impact: string = "";
   $: headerWidth;
 
   const screenshotRenderer = {
-    image(href:string, title:string, text:string) {
+    image(href: string, title: string, text: string) {
       return `
         <img class="w-full mx-auto overflow-hidden" src="${href}"" title="${title}" alt="${text}"/>
-      `
+      `;
     },
-    paragraph(text:string) {
-      return `${text}`
-    }
-  }
+    paragraph(text: string) {
+      return `${text}`;
+    },
+  };
 
   const impactRenderer = {
-    paragraph(text:string) {
+    paragraph(text: string) {
       return `
         <div class="flex flex-wrap relative rounded-sm my-4 mt-6 bg-gammaYellowLight">
           <div class="relative impact-icon w-[45px] rounded-sm bg-black">
@@ -34,41 +33,45 @@
           </div>
           <div class="px-2 py-1 w-[calc(100%-45px)]"><p class="my-0 py-0">${text}</-></div>
         </div>
-      `
-    }
-  }
+      `;
+    },
+  };
 
   const linksRenderer = {
-    image:defaultRenderer.image,
-    paragraph:defaultRenderer.paragraph,
-    link(href:string, title:string, text:string) {
+    image: defaultRenderer.image,
+    paragraph: defaultRenderer.paragraph,
+    link(href: string, title: string, text: string) {
       return `
         <a class="pl-2" href="${href}">${text}</a>
-      `
-    }
-  }
+      `;
+    },
+  };
 
   function initCustomMarked() {
-    marked.use({renderer:screenshotRenderer});
-    screenshots = marked.parse( data.screenshots );
-    marked.use({renderer:impactRenderer});
-    impact = marked.parse( data.impact );
-    marked.use({renderer:linksRenderer});
-    links = marked.parse( data.links );
+    marked.use({ renderer: screenshotRenderer });
+    screenshots = marked.parse(data.screenshots);
+    marked.use({ renderer: impactRenderer });
+    impact = marked.parse(data.impact);
+    marked.use({ renderer: linksRenderer });
+    links = marked.parse(data.links);
   }
 
-  onMount(()=>{
+  onMount(() => {
     initCustomMarked();
-  })
+  });
 </script>
 
-<svelte:window bind:innerWidth={headerWidth} /> 
+<svelte:window bind:innerWidth={headerWidth} />
 
-<div class="-z-10 -mt-9 md:-mt-11 -ml-12 md:-ml-16 h-[280px] md:h-[460px] md:-top-32 left-0 w-[calc(100%+80px)] md:w-[calc(100%+128px)] md:h-1/4">
+<div
+  class="-z-10 -mt-9 md:-mt-11 -ml-12 md:-ml-16 h-[280px] md:h-[460px] md:-top-32 left-0 w-[calc(100%+80px)] md:w-[calc(100%+128px)] md:h-1/4"
+>
   <img class="w-full h-full mx-auto object-cover" src={data.hero} />
-  <div class="absolute top-8 md:top-[60px] left-4 md:left-8 max-w-[240px] md:max-w-[360px] h-auto">
-    <img src={data.logo}/>
-  </div>    
+  <div
+    class="absolute top-8 md:top-[60px] left-4 md:left-8 max-w-[240px] md:max-w-[360px] h-auto"
+  >
+    <img src={data.logo} />
+  </div>
 </div>
 <div>
   <h1 class="mb-4">{data.title}</h1>
@@ -91,19 +94,19 @@
     <div>
       <h2 class="lg:hidden mt-6 lg:mt-0 mb-6">Screenshots</h2>
       <div class="screenshot-box">
-        {@html screenshots}
+        <SvelteMarkdown source={data.screenshots} />
       </div>
       <h3 class="mb-2">Links</h3>
       <div class="link-box bg-black px-4 py-2 mb-6">
         {@html links}
       </div>
     </div>
-  </div>  
+  </div>
   <div class="xl:hidden my-4 mt-8 mb-6">
     <h1>About {data.author}</h1>
     <SvelteMarkdown source={data.about} />
   </div>
-  <div class="my-4 ">
+  <div class="my-4">
     <h1 class="xl:mt-8 mb-4">Gamma Space Publishing</h1>
     <div class="rounded-md border-2 px-4 pb-4 border-black">
       <SvelteMarkdown source={data.gamma} />
