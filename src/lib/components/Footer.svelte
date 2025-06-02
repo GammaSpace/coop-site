ea94b9df350fcf24700d93e0d079795659024b22
Jennie Robinson Faber <jennie@jenniefaber.com>
2025-06-02 14:36:15 -0400

    Remove phone number

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,11 +9,7 @@
     class="max-w-screen-xl mx-auto pt-4 pb-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
   >
     <div class="flex justify-center md:order-2">
-      <a href="tel:6476896005" class="flex">
-        <span class="sr-only">Phone</span>
-        {@html phone}
-        <span class="ml-3 mr-6">(647) 689-6005</span>
-      </a>
+
       <a
         href="mailto:info@gammaspace.ca"
         class="text-black hover:text-gray-500"
